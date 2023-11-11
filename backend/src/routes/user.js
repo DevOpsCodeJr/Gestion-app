969b6ed82d5cddbf30ae9a1e4e3492b3dd73569b
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2023-11-11 16:15:24 -0300

    Fixing auth on user routes.

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -4,7 +4,7 @@ import { register, login, update } from "../controllers/user.js";
 
 const router = express.Router();
 
-router.post("/register", auth, register);
+router.post("/register", register);
 router.post("/login", login);
 router.put("/update", auth, update);
 
