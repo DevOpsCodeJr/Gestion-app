5ff58db42509a86963f8bf6cbc23927b94866a52
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2023-11-18 16:11:40 -0300

    Agregando Auth.

--- gestion-app-frontend/src/context/AppContext.jsx
+++ gestion-app-frontend/src/context/AppContext.jsx
@@ -6,12 +6,27 @@ export const AppContext = createContext();
 export const initialState = { theme: !!localStorage.theme }
 
 export const AppProvider = ({ children }) => {
+    const [auth, setAuth] = useState({});
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-
+        authUser();
     }, [])
 
+    const authUser = async () => {
+        const token = localStorage.getItem("token");
+        const user = localStorage.getItem("user");
+    
+        const userObject = JSON.parse(user);
+    
+        if (!token || !user) {
+          setLoading(false);
+          return false;
+        }
+    
+        setAuth(userObject);
+        setLoading(false);
+      };
 
     const reducer = (state, action) => {
         switch (action.type) {
@@ -25,7 +40,7 @@ export const AppProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
-        <AppContext.Provider value={{ users, loading, setLoading, state, dispatch, }}>
+        <AppContext.Provider value={{ auth, setAuth, loading, setLoading, state, dispatch, }}>
             {children}
         </AppContext.Provider>
     );
